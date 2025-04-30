bb3767f28c44bf691ba787524406720c9d608cc3
Marcelo-Gabriel <00160800biel@gmail.com>
2025-04-30 10:15:14 -0300

    primeiro commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const v = require("vercel.json")
 
 const app = express();
 const port = 3000;
